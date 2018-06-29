3e6511bee3196491a91e44e77b01686854e14239
Bouchnafa, Thami <thami.bouchnafa@gmail.com>
2018-06-29 19:37:18 +0200

    Support update commands

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const getEnv = require('./lib/getEnv');
 const Redis = require('ioredis');
 const mime = require('mime-types');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const host = getEnv('REDIS_HOST', '127.0.0.1');
 const redisPort = getEnv('REDIS_PORT', 6379);
@@ -17,27 +18,68 @@ app.get('/', (req, res) => {
     res.end(JSON.stringify({status: 'up', uptime: process.uptime(), description: "Http wrapper for ioredis"}, null, 2));
 });
 
+app.use(bodyParser.json());
+
 app.get('/:command/:key*', (req, res) => {
-    const key = req.params.key + (req.params['0'] || '');
-    getCommandHandler(req.params.command)(key, res, req.params.command);
+    const key = `${req.params.key}${req.params['0']}`;
+    getFetchCommandHandler(req.params.command)(key, res, req.params.command);
 });
 
-const getCommandHandler = (command) => commandHandler[command] || commandHandler._;
+app.put('/:command/:key*', (req, res) => {
+    const key = `${req.params.key}${req.params['0']}`;
+    getUpdateCommandHandler(req.params.command)(key, res, req.params.command, req.body);
+});
 
-const commandHandler = {
+const getFetchCommandHandler = (command) => fetchCommandsHandler[command] || fetchCommandsHandler._default;
+
+const getUpdateCommandHandler = (command) => updateCommandHandler[command] || updateCommandHandler._default;
+
+const fetchCommandsHandler = {
     keys: (key, res) => {
         redis.keys(key, (err, result) => {
+            if (err) {
+                res.status(500);
+                res.end(err.message);
+                return;
+            }
             res.status(200);
             res.contentType(mime.lookup('json'));
             res.end(JSON.stringify(result, null, 2));
         })
     },
-    _: (key, res, command) => {
+    _default: (key, res, command) => {
         redis[command](key, (err, result) => {
+            if (err) {
+                res.status(500);
+                res.end(err.message);
+            }
             res.status(200);
             res.contentType(mime.lookup('json'));
-            res.end(JSON.stringify(JSON.parse(result), null, 2));
+            res.end(result && JSON.stringify(JSON.parse(result), null, 2) || result);
         })
+    }
+};
+
+const updateCommandHandler = {
+    _default: (key, res, command, body) => {
+        try {
+            const value = JSON.stringify(body);
+            redis[command](key, value, function (err) {
+                if (err) {
+                    console.error(err.message);
+                    res.status(500);
+                    res.end(err.message);
+                    return;
+                }
+                res.status(200);
+                res.contentType(mime.lookup('json'));
+                res.end(JSON.stringify({key: key, value: body}, null, 2));
+            })
+        } catch (e) {
+            console.error(e.message);
+            res.status(500);
+            res.end(err.message);
+        }
     }
 };
 
